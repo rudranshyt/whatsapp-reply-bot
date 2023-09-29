8d65deb84fed581fe83e5b530c0f6c1fb84c4886
rudranshyt <90946946+rudranshyt@users.noreply.github.com>
2023-09-29 19:43:42 +0530

    initial commit

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pywhatkit
 from config import phone
 
+phoneNumber = input('enter a number')
 pywhatkit.sendwhatmsg_instantly(
     phoneNo = phone,
     message="Howdy! This message will be sent instantly!",
